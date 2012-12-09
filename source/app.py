78e762d2a36117901be856e10d56d9f52210607e
Alireza <alireza@Alireza-Ubuntu.(none)>
2012-12-09 23:12:42 +0330

    fix posts order in feed

--- source/app.py
+++ source/app.py
@@ -26,8 +26,9 @@ def getRajaLastNews():
 	img = tree.xpath('//table[@id="ShowRotateNews"]//img')[0]
 	posts.append({'title': title.text, 'subtitle': '', 'description': desc.get('title').strip(), 'link': domain + title.get('href'), 'image': domain + img.get('src'), 'published': datetime.datetime.now()})
 
-	# last posts
-	for i in [1, 4, 7, 10]:
+	# last n posts
+	n = 5
+	for i in range(1, 3*n, 3):
 		title = tree.xpath('//table[@id="NewsWithImage1"]/tr['+ str(i) +']//div[@class="Titr2"]/a')[0]
 		subtitle = tree.xpath('//table[@id="NewsWithImage1"]/tr['+ str(i) +']//div[@class="Titr1"]/a')[0]
 		img = tree.xpath('//table[@id="NewsWithImage1"]/tr['+ str(i+1) +']//img')[0]
@@ -43,7 +44,7 @@ def main_feed():
 
 	feed = AtomFeed(u'خبرخوان رجا نیوز', feed_url=Request.url, url=Request.url_root)
 
-	for post in posts.find().limit(10):
+	for post in posts.find().sort('published', -1).limit(20):
 		content = '<img style="float: right; margin-left: 15px; width: 80px" src="%s"><p style="color: #777">%s</p><p>%s</p>' % (post['image'], post['subtitle'] if post['subtitle'] else '', post['description'])
 		feed.add(post['title'], unicode(content), content_type='html', url=post['link'], updated=post['published'], published=post['published'])
 
