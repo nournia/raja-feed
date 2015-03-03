5efcafd9ff6c29dd08344e19cb58fc91f36972b1
Alireza Nourian <az.nourian@gmail.com>
2015-03-03 15:40:22 +0330

    make feed generator compatible with redesigned site

--- main.py
+++ main.py
@@ -3,36 +3,38 @@
 import datetime
 from flask import Flask, request
 from werkzeug.contrib.atom import AtomFeed
-from lxml import etree
+from pyquery import PyQuery as pq
 
 app = Flask(__name__)
 
 
-def getRajaLastNews(n=10):
+def getRajaLastNews():
 	domain = 'http://www.rajanews.com/'
-	tree = etree.parse(domain, etree.HTMLParser())
+	raja = pq(domain)
 
-	posts = []
+	item = raja('.slider1 .item')
+	yield {
+		'title': item.find('.title').text(),
+		'subtitle': item.find('.top-title').text(),
+		'description': item.find('.lead').text(),
+		'link': domain + item.find('a').attr('href'),
+		'image': item.find('img').attr('src'),
+		'published': datetime.datetime.now()
+	}
 
-	# featured post
-	title = tree.xpath('//table[@id="ShowRotateNews"]//div[@class="Titr2"]/a')[0]
-	desc = tree.xpath('//table[@id="ShowRotateNews"]//td')[0]
-	img = tree.xpath('//table[@id="ShowRotateNews"]//img')[0]
-	posts.append({'title': title.text, 'subtitle': '', 'description': desc.get('title').strip(), 'link': domain + title.get('href'), 'image': domain + img.get('src'), 'published': datetime.datetime.now()})
-
-	# last n posts
-	for i in range(1, 3*n, 3):
-		title = tree.xpath('//table[@id="NewsWithImage1"]/tr['+ str(i) +']//div[@class="Titr2"]/a')[0]
-		subtitle = tree.xpath('//table[@id="NewsWithImage1"]/tr['+ str(i) +']//div[@class="Titr1"]/a')[0]
-		img = tree.xpath('//table[@id="NewsWithImage1"]/tr['+ str(i+1) +']//img')[0]
-		desc = tree.xpath('//table[@id="NewsWithImage1"]/tr['+ str(i+1) +']//div[@class="Lead"]')[0]
-		posts.append({'title': title.text, 'subtitle': subtitle.text, 'description': desc.text.strip(), 'link': domain + title.get('href'), 'image': domain + img.get('src'), 'published': datetime.datetime.now()})
-
-	return posts
+	for item in raja('.homepage .item').items():
+		yield {
+			'title': item.find('.title').text(),
+			'subtitle': item.find('.top-title').text(),
+			'description': item.find('.lead').text(),
+			'link': domain + item.find('a').attr('href'),
+			'image': item.find('img').attr('src'),
+			'published': datetime.datetime.now()
+		}
 
 
 def getFeed(posts):
-	feed = AtomFeed(u'خبرخوان رجا نیوز', feed_url=request.url, url=request.url_root, icon='http://www.rajanews.com/favicon.ico', author={'name': u'سُبحه', 'uri': 'www.sobhe.ir'})
+	feed = AtomFeed(u'خبرخوان رجا نیوز', feed_url=request.url, url=request.url_root, icon='http://www.rajanews.com/favicon.ico', author={'name': u'رجا نیوز', 'uri': 'www.rajanews.com'})
 
 	for post in posts:
 		content = '<img style="float: right; margin-left: 15px; width: 80px" src="%s"><p style="color: #777">%s</p><p>%s</p>' % (post['image'], post['subtitle'] if post['subtitle'] else '', post['description'])
